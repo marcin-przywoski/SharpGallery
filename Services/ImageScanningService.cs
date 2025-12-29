2aa1983bf0e386b97d1ea9f2def0710a745416dd
Marcin Przywóski <marcin.przywoski26@gmail.com>
2025-12-29 23:53:35 +0100

    Added ImageScanningService.cs

---
+++ Services/ImageScanningService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Avalonia.Media.Imaging;
+using SharpGallery.Models;
+using SkiaSharp;
+
+namespace SharpGallery.Services
+{
+    public class ImageScanningService
+    {
+
+    }
+}
