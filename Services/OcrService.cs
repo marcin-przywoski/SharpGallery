fb474476c9decaa88af4f1fe9743e24f651e75aa
Marcin Przywóski <marcin.przywoski26@gmail.com>
2026-01-09 20:46:08 +0100

    Added OcrService.cs

---
+++ Services/OcrService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using SharpGallery.Models;
+using Tesseract;
+
+namespace SharpGallery.Services
+{
+    public class OcrService
+    {
+        
+    }
+    
+}
