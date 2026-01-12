8ae805d8578575bfbe36b556516b2ba4afa80547
Marcin Przywóski <marcin.przywoski26@gmail.com>
2026-01-13 00:09:55 +0100

    Added method for initializing the OCR service to OcrService.cs

--- Services/OcrService.cs
+++ Services/OcrService.cs
@@ -15,6 +15,45 @@ namespace SharpGallery.Services
         private const string TesseractDataUrl = "https://github.com/tesseract-ocr/tessdata_best/raw/main/eng.traineddata";
 
         private readonly string ApplicationFolder = AppContext.BaseDirectory + @"\tessdata";
+
+        public async Task InitializeAsync(string dataPath)
+        {
+            if (_isLoaded)
+                return;
+
+            await Task.Run(async () =>
+            {
+                try
+                {
+                    if (!Directory.Exists(dataPath))
+                    {
+                        Directory.CreateDirectory(dataPath);
+                    }
+
+                    string tesseractFile = Path.Combine(dataPath, "eng.traineddata");
+
+                    if (!File.Exists(tesseractFile))
+                    {
+                        using var client = new HttpClient();
+                        Console.WriteLine("Downloading Tesseract data...");
+                        using var response = await client.GetAsync(TesseractDataUrl);
+                        response.EnsureSuccessStatusCode();
+                        using var stream = await response.Content.ReadAsStreamAsync();
+                        using var fileStream = File.Create(tesseractFile);
+                        await stream.CopyToAsync(fileStream);
+                        Console.WriteLine("Downloaded Tesseract data.");
+                    }
+
+                    _engine = new TesseractEngine(dataPath, "eng", EngineMode.LstmOnly);
+                    _isLoaded = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Failed to init OCR: {ex.Message}");
+                }
+            });
+        }
+
     }
     
 }
